3dc45c7544fd26be9f9ec198feb7393cd6e2b1eb
nilton santos <nilton.santos@ucsp.edu.pe>
2020-09-19 16:00:33 -0500

    subiendo archivosfinales

---
+++ precache-manifest.4b7b63513b81ed865867f5cda1f14e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350f8daaaa869baaf52d4e2f597fdea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceabf3d61946d44c89fa",
+    "url": "/static/css/main.b0c0153d.chunk.css"
+  },
+  {
+    "revision": "138ae2886d49b174d92f",
+    "url": "/static/js/2.f94f828f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f94f828f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceabf3d61946d44c89fa",
+    "url": "/static/js/main.8d52e095.chunk.js"
+  },
+  {
+    "revision": "537c2da7cfcf6e229818",
+    "url": "/static/js/runtime-main.9abc6f97.js"
+  }
+]);
